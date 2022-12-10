84ddf32d915d77879c4bd9bc02e3b47117e4e667
Issac <30824579+Areck83@users.noreply.github.com>
2022-12-09 20:03:42 -0600

    Creacion inicial del maven
    Actualizado a la version de compilador 1.8, dependencias necesarias agregadas

---
+++ AppDB/src/main/java/mx/equipo9uv/App.java
@@ -0,0 +1,13 @@
+package mx.equipo9uv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
